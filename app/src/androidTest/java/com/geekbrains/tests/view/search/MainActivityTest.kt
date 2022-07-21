3500cd37bcb8d8418ea6d47e0822bd5b3f9afdf4
zoer <zoer.aleksandr@gmail.com>
2022-07-21 20:28:08 +0300

    Отредактировал сгенерированный тест MainActivityTest.kt

--- app/src/androidTest/java/com/geekbrains/tests/view/search/MainActivityTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/view/search/MainActivityTest.kt
@@ -1,7 +1,5 @@
 package com.geekbrains.tests.view.search
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -11,10 +9,7 @@ import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.geekbrains.tests.R
 import com.geekbrains.tests.TEST_NUMBER_OF_RESULTS_PLUS_1
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -29,70 +24,30 @@ class MainActivityTest {
 
     @Test
     fun mainActivityTest() {
-        val materialButton = onView(
+        val toDetailsButton = onView(
             allOf(
                 withId(R.id.toDetailsActivityButton), withText("to details"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
                 isDisplayed()
             )
         )
-        materialButton.perform(click())
+        toDetailsButton.perform(click())
 
-        val materialButton2 = onView(
+        val incrementButton = onView(
             allOf(
                 withId(R.id.incrementButton), withText("+"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
                 isDisplayed()
             )
         )
-        materialButton2.perform(click())
+        incrementButton.perform(click())
 
         val textView = onView(
             allOf(
                 withId(R.id.totalCountTextView), withText(TEST_NUMBER_OF_RESULTS_PLUS_1),
-                withParent(withParent(withId(android.R.id.content))),
                 isDisplayed()
             )
         )
         textView.check(matches(withText(TEST_NUMBER_OF_RESULTS_PLUS_1)))
 
-        val button = onView(
-            allOf(
-                withId(R.id.incrementButton), withText("+"),
-                withParent(withParent(withId(android.R.id.content))),
-                isDisplayed()
-            )
-        )
-        button.check(matches(isDisplayed()))
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
+        incrementButton.check(matches(isDisplayed()))
     }
 }
